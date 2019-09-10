56f673c1d729f7a12529de721c227ecfdb8382da
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-10 22:15:45 +0100

    Added API call to NASA APOD and simple UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+function App({ apiKey }) {
+	const [data, setData] = useState({})
+	const fetchApiData = () => {
+		axios.get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+			.then(response => {
+				console.log(response.data);
+				setData(response.data);
+			})
+			.catch(error => console.error(error));
+		return false;
+	}
+	useEffect(() => {
+		fetchApiData();
+		return () => console.log('cleanup')
+	}, [])
+	return (
+		<div className="App">
+			<p>
+				Read through the instructions in the README.md file to build your NASA
+				app! Have fun 🚀!
+			</p>
+			{
+				!data ? 
+				<div className="loading">Loading...</div> : 
+				<div className="content">
+					<h2>{data.title}</h2>
+					<p>{data.date}</p>
+					<p>{data.explanation}</p>
+					<img src={data.url} />
+					{/* <img src={data.hdurl} /> */}
+				</div>
+			}
+		</div>
+	);
 }
 
 export default App;
