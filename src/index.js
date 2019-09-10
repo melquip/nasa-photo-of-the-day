94f3de5155d50b48d9fc3b15bd15e73887766bf2
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-10 22:15:12 +0100

    Added API_KEY and Axios to the project

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const NASA_APOD_API_KEY = process.env.REACT_APP_API_KEY;
+ReactDOM.render(<App apiKey={NASA_APOD_API_KEY} />, document.getElementById("root"));
