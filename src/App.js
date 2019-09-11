b8ba5aca44f85e336851422aef4b55dc3f1e7f3e
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-11 15:43:26 +0100

    Finished stretch goals

--- src/App.js
+++ src/App.js
@@ -8,51 +8,81 @@ import { APIImage } from "./APIImage";
 
 
 function App({ apiKey }) {
-	const [info, setInfo] = useState({});
+	const [apod, setAPOD] = useState({});
+	const [neo, setNeo] = useState({});
 
 	let today = new Date().toISOString().substr(0, 10);
 	const [currDate, setDate] = useState(today);
 	
 	useEffect(() => {
-		axios.get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&hd=false&date=${currDate}`)
-			.then(response => {
-				console.log(response.data);
-				setInfo(response.data);
-			})
-			.catch(error => console.error(error));
-		return () => setInfo({})
+		const apodPromise = axios.get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&hd=false&date=${currDate}`);
+		const neoPromise = axios.get(`https://api.nasa.gov/neo/rest/v1/feed?api_key=${apiKey}&start_date=${currDate}&end_date=${currDate}`);
+		Promise.all([apodPromise, neoPromise])
+		.then(([apodRes, neoRes]) => {
+			console.log("\n\nAPOD", apodRes.data);
+			setAPOD(apodRes.data);
+			console.log("\n\nNEO", neoRes.data);
+			setNeo(neoRes.data);
+		}).catch(error => console.error(error));
+		return () => {
+			setAPOD({})
+			setNeo({})
+		}
 	}, [apiKey, currDate]);
 	return (
 		<div className="App">
 			{
-				!info ?
+				!apod ?
 					<div className="loading">Loading...</div> :
 					<div className="content">
 						<div className="header">
-							<APITitle text={info.title} />
+							<APITitle text={apod.title} />
 							<APIDate date={currDate} setDate={setDate} today={today} />
 						</div>
 						<div className="flex">
-							<APISummary text={info.explanation} />
+							<APISummary text={apod.explanation} />
 							{
-								info.media_type === 'image' ? 
+								apod.media_type === 'image' ? 
 								<APIImage 
 									hd={false} 
-									source={info.url} 
-									text={info.title}
+									source={apod.url} 
+									text={apod.title}
 								/> : 
-								<iframe src={info.url} title={info.title}></iframe>
+								<iframe src={apod.url} title={apod.title}></iframe>
 							}
 						</div>
 						{
-							info.media_type === 'image' ? 
+							apod.media_type === 'image' ? 
 							<APIImage 
 								hd={true} 
-								source={info.hdurl} 
-								text={info.title}
+								source={apod.hdurl} 
+								text={apod.title}
 							/> : null
 						}
 					</div>
+			}
+			{
+				!neo || !neo.near_earth_objects ? <div className="loading">Loading...</div> :
+				<div className="content">
+					<div className="header">
+						<APITitle text="Near Earth Objects" />
+						<p>{neo.element_count} NEOs in {currDate}</p>
+					</div>
+					{
+						neo.near_earth_objects[currDate.toString()] ? neo.near_earth_objects[currDate.toString()].map(asteroid => {
+							console.log(asteroid);
+							return (
+								<div className="asteroid" key={asteroid.neo_reference_id}>
+									<p>Object ID: {asteroid.neo_reference_id}</p>
+									<p>Object Name: {asteroid.name}</p>
+									<p>Object Data: <a href={asteroid.links.self} target="_blank" rel="noopener noreferrer">Click here</a></p>
+									<p>Is Hazardous: {asteroid.is_potentially_hazardous_asteroid ? 'Yes' : 'No'}</p>
+									<p>Is Sentry: {asteroid.is_sentry_object ? 'Yes' : 'No'}</p>
+								</div>
+							)
+						}) : null
+					}
+				</div>
 			}
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-const NASA_APOD_API_KEY = process.env.REACT_APP_API_KEY;
-ReactDOM.render(<App apiKey={NASA_APOD_API_KEY} />, document.getElementById("root"));
+const NASA_API_KEY = process.env.REACT_APP_API_KEY;
+ReactDOM.render(<App apiKey={NASA_API_KEY} />, document.getElementById("root"));
